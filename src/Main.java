2de84d74d0336a26cacafad14d1e0084577d5c85
irhry@smile.fr <gagarina67942gmail.com>
2019-05-29 09:40:55 +0300

    Tenth

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<String> greeting = new ArrayList<>();
+        greeting.add(Tenth.getLine());
     }
 }
---
+++ src/Tenth.java
@@ -0,0 +1,5 @@
+public class Tenth {
+    public static String getLine() {
+        return "(Без него никак нельзя),";
+    }
+}
